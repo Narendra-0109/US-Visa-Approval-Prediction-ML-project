8a3bd3f21801120051d826579ba0bab4fe6c192e
Narendra-0109 <teenie0109@gmail.com>
2025-10-07 11:30:28 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="teenie",
+    author_email="teenie0109@gmail.com",
+    packages=find_packages()
+)
